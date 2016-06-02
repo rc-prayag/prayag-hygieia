af30f687975431d6e631cd4463ab060737c00dc5
rc-prayag <prayag.c@royalcyber.com>
2016-06-02 17:31:53 +0530

    Update Server.java

--- api/src/main/java/com/capitalone/dashboard/model/deploy/Server.java
+++ api/src/main/java/com/capitalone/dashboard/model/deploy/Server.java
@@ -2,6 +2,7 @@ package com.capitalone.dashboard.model.deploy;
 
 public class Server {
     private final String name;
+    
     private final boolean online;
 
     public Server(String name, boolean online) {
